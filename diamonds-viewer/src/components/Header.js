c8e79cff393d7a4bf69badfd1964aea3b0cf8414
Nina M <manina103@hotmail.com>
2017-06-07 14:22:09 +0200

    frontend stuff + eject from create-react-app

---
+++ diamonds-viewer/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/header.css'
+
+const Header = () => {
+  return(
+    <h1 className={styles.header}>Diamant Spelet</h1>
+  );
+}
+
+export default Header;
