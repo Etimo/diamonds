71bd7eeeb3df2dc562305d0fa08142ff298b6ac6
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2017-08-30 12:46:24 +0200

    Auto set id for bot

--- diamonds-api/Diamonds.Common/Entities/Bot.cs
+++ diamonds-api/Diamonds.Common/Entities/Bot.cs
@@ -7,7 +7,7 @@ namespace Diamonds.Common.Entities
 {
     public class Bot
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; }
         public string Email { get; set; }
         public string Token { get; set; } = Guid.NewGuid().ToString();
