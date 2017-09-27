1e7f6c6268712ea2504593e10666929985cdc544
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2017-09-27 15:55:39 +0200

    Add script for running bot on server

---
+++ diamonds-bot-example/auto_play.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+HOST=http://diamonds.etimo.se/api
+PYTHON=python3
+if [ ! -f ".auto_token" ]; then
+    echo "Generating bot and token..."
+    TOKEN=$($PYTHON main.py --host $HOST --email bot@etimo.se --name Etimo --logic FirstDiamond | grep "Bot registered" | awk '{print $4}')
+    if [ "$TOKEN" == "" ]; then
+        echo "Unable to get token."
+        exit 1
+    fi
+    echo $TOKEN > .auto_token
+    echo "Done. Got token $TOKEN"
+else
+    TOKEN=$(cat .auto_token)
+fi
+
+echo "Playing using token: $TOKEN"
+$PYTHON main.py --host $HOST --token $TOKEN --logic FirstDiamond
