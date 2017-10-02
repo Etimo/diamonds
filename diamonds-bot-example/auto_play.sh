5e435b6752344152f03ef5b6baa234d420bf9dba
Oscar Nylander <oscar.nylander@etimo.se>
2017-10-02 17:45:07 +0200

    Made the bot deployable through docker-compose and made it work with a persistent token in a container

--- diamonds-bot-example/auto_play.sh
+++ diamonds-bot-example/auto_play.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 HOST=http://diamonds.etimo.se/api
 PYTHON=python3
-if [ ! -f ".auto_token" ]; then
+if [ ! -f "./token/.auto_token" ]; then
     echo "Generating bot and token..."
     TOKEN=$($PYTHON main.py --host $HOST --email bot@etimo.se --name Etimo --logic FirstDiamond | grep "Bot registered" | awk '{print $4}')
     if [ "$TOKEN" == "" ]; then
         echo "Unable to get token."
         exit 1
     fi
-    echo $TOKEN > .auto_token
+    echo $TOKEN > ./token/.auto_token
     echo "Done. Got token $TOKEN"
 else
-    TOKEN=$(cat .auto_token)
+    TOKEN=$(cat ./token/.auto_token)
 fi
 
 echo "Playing using token: $TOKEN"
