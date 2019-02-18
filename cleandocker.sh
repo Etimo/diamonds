4aae45383bfd7dc43371e993c7f3e74162457114
Teo Klestrup Röijezon <teo@nullable.se>
2019-02-18 15:21:48 +0100

    Removed cleandocker.sh
    It's *very* unsafe (it will remove images indiscriminately, and ***PRUNES YOUR
    VOLUMES***). Use `docker system prune --all` (with `--volumes` if you *really*
    want to remove those too) instead.

--- cleandocker.sh
+++
@@ -1,9 +0,0 @@
-function cleanByString {
-    echo Cleaning all containers and images matching string: $1
-docker ps -a|grep $1|awk '{print $1}'|xargs docker stop
-docker ps -a|grep $1|awk '{print $1}'|xargs docker rm
-docker images|grep $1|awk '{print $3}'|xargs docker image rm
-}
-cleanByString diamond
-cleanByString none
-docker volume prune
